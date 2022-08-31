7dc7ff0d6ee8f61557a441060157dca46a39cf38
TrippinSlim <104782855+TrippinSlim@users.noreply.github.com>
2022-08-31 09:24:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\TemplateController;
 
 /*
